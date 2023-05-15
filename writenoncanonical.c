d8f8325efc12b3a009dda216fc12827bb38444a6
Ana Pinto <up202005261@fe.up.pt>
2023-05-15 14:19:51 +0100

    ficheiros novos e control frame começado

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define BAUDRATE B9600
 #define MODEMDEVICE "/dev/ttyS1"
@@ -15,18 +16,110 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG 0x5c   // header delimitation
+#define A    0x01   //Answer sent by receiver
+#define C_UA    0x07   //Control field - UA
+#define C_SET   0x03   //Control field - SET
+#define BCC_UA  (A^C_UA)
+#define BCC_SET (A^C_SET)
+
+#define FRAME_SIZE 5
+
 volatile int STOP=FALSE;
 
-int main(int argc, char** argv)
-{
-    int fd,c, res;
+int send_command(int fd, unsigned int* command){
+
+    int res;
+
+    res = write(fd,command,FRAME_SIZE);            //returns number of written bytes in the driver file and saves in res
+    printf("%d BYTES SENT\n", res);
+
+    if (res == 5) return 1;
+
+}
+
+int read_answer(int fd, unsigned int* answer, unsigned int* recv){
+
+    int FLAG_ANSWER = answer[0];
+    int A_ANSWER = answer[1];
+    int C_ANSWER = answer[2];
+    int BCC_ANSWER = answer[3];
+
+    //Estados
+    typedef enum {
+        START,
+        FLAG_RCV,
+        A_RCV,
+        C_RCV,
+        BCC_OK,
+        STOP
+    } statesSET;
+
+    statesSET state;
+    int i;
+
+    for(i=0; i<FRAME_SIZE;i++){
+        read(fd,recv+i,1); //reads chars one by one
+
+        printf("BYTE LIDO %d\n", recv[i]);
+
+        switch(state){
+
+		    case START:
+		        printf("start\n");
+		        if(recv[i]==FLAG_ANSWER){
+			        state=FLAG_RCV;
+		        }
+		        else state=START; 
+		    break;
+		   
+            case FLAG_RCV:
+		        printf("flag_rcv\n");
+		        if(recv[i]==A_ANSWER) state = A_RCV;
+		        if (recv[i] == FLAG_ANSWER) state = FLAG_RCV;
+                else state = START;
+		    break;
+
+            case A_RCV:
+		        printf("a_rcv\n");
+                if(recv[i]==C_ANSWER) state = C_RCV;
+		        if(recv[i] == FLAG_ANSWER) state = FLAG_RCV;
+                else state = START;
+		    break;
+
+            case C_RCV:
+		        printf("c_rcv\n");
+                if(recv[i]==BCC_ANSWER) state = BCC_OK;
+		        if(recv[i] == FLAG_ANSWER) state = FLAG_RCV;
+                else state = START;
+		    break;
+
+            case BCC_OK:
+		        printf("bcc_ok\n");
+                if(recv[i] == FLAG_ANSWER) state = STOP;
+                else state = START;
+		    break;
+
+            case STOP:
+		        printf("stop\n");
+		    break;
+
+		   
+        }
+        if (state==STOP) break;
+    }
+    recv[i+1] = 0; //so we can printf
+    printf("COMMAND RECEIVED: %s\n",recv);
+
+    return 1;
+}
+
+int main(int argc, char** argv){
+    int fd,c;
     struct termios oldtio,newtio;
-    char buf[255];
     int i, sum = 0, speed = 0;
 
-    if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&         //se for diferente de /dev/ttyS0 ou /dev/ttyS1 
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    if ( (argc < 2) || ((strcmp("/dev/ttyS0", argv[1])!=0) &&  (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
@@ -57,8 +150,6 @@ int main(int argc, char** argv)
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
-
-
     /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
     leitura do(s) próximo(s) caracter(es)
@@ -74,25 +165,14 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    /*SENDING STRING*/
-    for (i = 0; i < 255; i++) {     //reads 
-        buf[i] = getchar();
-        if (buf[i] == '\n' || buf[i] == '\0') break;
-    }
+    //SEND SET COMMAND
+    unsigned int set[FRAME_SIZE] =  {FLAG, A, C_SET, BCC_SET, FLAG};
+    if (send_command(fd,set) != 1) printf("ERROR SENDING %s COMMAND\n",set);
 
-    printf("STRING SENT: %s",buf);
-    res = write(fd,buf,i+1);            //returns number of written bytes in the driver file and saves in res
-    printf("%d bytes written\n", res);
-
-
-    /*RECEIVING IT BACK*/
-    for(i=0; i<255;i++){
-        read(fd,buf+i,1); //reads chars one by one
-        if (buf[i] == 'z') break;
-    }
-
-    buf[i+1] = 0; //so we can printf
-    printf("STRING RECEIVED: %s\n",buf);
+    //RECEIVE UA ANSWER
+    unsigned int ua[FRAME_SIZE] = {FLAG, A, C_UA, BCC_UA, FLAG};
+    unsigned recv[FRAME_SIZE];
+    read_answer(fd,ua,recv);
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {     //set attributes again
         perror("tcsetattr");
