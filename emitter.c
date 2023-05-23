d13dab129d7fc3b84de95301cf12b177b6ecacff
Ana Pinto <up202005261@fe.up.pt>
2023-05-23 03:39:41 +0100

    Byte Stuffing and de-stuffing working

--- emitter.c
+++ emitter.c
@@ -16,53 +16,102 @@
 #define FALSE 0
 #define TRUE 1
 
-#define FLAG 0x5c   // header delimitation
-#define A    0x01   //Answer sent by receiver
-#define C_UA    0x07   //Control field - UA
-#define C_SET   0x03   //Control field - SET
-#define BCC_UA  (A^C_UA)
-#define BCC_SET (A^C_SET)
+#define FLAG 0x5c  // header delimitation
+#define A 0x01     // Answer sent by receiver
+#define C_UA 0x07  // Control field - UA
+#define C_SET 0x03 // Control field - SET
+#define BCC_UA (A ^ C_UA)
+#define BCC_SET (A ^ C_SET)
 
 #define FRAME_SIZE 5
 #define TIME_OUT 5
 
 int timer_on = 0, alarm_counter = 0;
 int fd;
-char set[FRAME_SIZE] =  {FLAG, A, C_SET, BCC_SET, FLAG};
+char set[FRAME_SIZE] = {FLAG, A, C_SET, BCC_SET, FLAG};
 char ua[FRAME_SIZE] = {FLAG, A, C_UA, BCC_UA, FLAG};
 
+int write_frame(int fd, char *header)
+{   
+    char dados[7] ={0x5D, 0x5c, 0x01, 0x03, 0x02, 0x5C};
 
-void flag_time_out(){ 
+    //calcula BCC2
+    char bcc2 = dados[0];
+
+    for (int i = 1; i < sizeof(dados); i++){
+        bcc2 = dados[i] ^ bcc2;
+    }
+    printf("\tBCC2: 0x%x\n", bcc2);
+
+    
+    //envia header
+    write(fd,header,strlen(header));
+    //strcat(dados,bcc2_str);
+    dados[6] = bcc2;
+
+    //envia dados + bcc2
+    for(int j = 0; j < sizeof(dados); j++)
+    {   
+        //printf("%x ",dados[j]);
+        if(dados[j] == 0x5C){
+            char stuff_C[2] = {0x5D, 0x7C};  
+            printf("%x ",stuff_C[0]);  printf("%x ",stuff_C[1]);  
+            write(fd, stuff_C, 2);  
+        }
+
+        else if(dados[j] == 0x5D){ 
+            char stuff_D[2] = {0x5D, 0x7D};
+            printf("%x ",stuff_D[0]);  printf("%x ",stuff_D[1]);
+            write(fd, stuff_D, 2);
+        }
+        else{
+            write(fd,dados+j,1);
+            printf("%x ",dados[j]);
+        }
+
+    }
+    
+    //envia flag
+    write(fd,header,1);
+
+    return 1; 
+}
+void flag_time_out()
+{
     timer_on = 0;
     alarm_counter++;
-    if (alarm_counter > 3) {
+    if (alarm_counter > 3)
+    {
         printf("Timed out for the third time\n");
         exit(1);
-    } 
+    }
     printf("Alarm timed out\n Retransmitting...\n");
-    send_command(fd,set);
+    send_command(fd, set);
 }
 
-int send_command(int fd, char* command){
+int send_command(int fd, char *command)
+{
 
     int res;
 
-    res = write(fd,command,FRAME_SIZE);            //returns number of written bytes in the driver file and saves in res
+    res = write(fd, command, FRAME_SIZE); // returns number of written bytes in the driver file and saves in res
     printf("%d BYTES SENT\n", res);
 
-    if (res == 5) return 1;
-
+    if (res == 5)
+        return 1;
 }
 
-int read_answer(int fd, char* answer){
+int read_answer(int fd, char *answer)
+{
 
     char FLAG_ANSWER = answer[0];
     char A_ANSWER = answer[1];
     char C_ANSWER = answer[2];
     char BCC_ANSWER = answer[3];
 
-    //Estados
-    typedef enum {
+    // Estados
+    typedef enum
+    {
         START,
         FLAG_RCV,
         A_RCV,
@@ -76,84 +125,118 @@ int read_answer(int fd, char* answer){
 
     char byte;
 
-    while(1){
+    while (1)
+    {
 
-        if (!timer_on) {
-            alarm(TIME_OUT);  // ativa alarme de TIME_OUT segundos
+        if (!timer_on)
+        {
+            alarm(TIME_OUT); // ativa alarme de TIME_OUT segundos
             printf("Alarm set\n");
-            timer_on=1;
+            timer_on = 1;
         }
 
+        read(fd, &byte, 1); // reads chars one by one
 
-        read(fd,&byte,1); //reads chars one by one
+        // printf("\nVOLTA : %d ---- Byte lido: %x\n", i,byte);
 
-        //printf("\nVOLTA : %d ---- Byte lido: %x\n", i,byte);
+        switch (state)
+        {
 
-        switch(state){
-
-		    case START:
-		        printf("start\n");
-		        if(byte==FLAG_ANSWER) state=FLAG_RCV;
-		        else state=START; 
-		    break;
-		   
-            case FLAG_RCV:
-		        printf("flag_rcv\n");
-		        if(byte==A_ANSWER){ state = A_RCV; break;}
-		        if (byte == FLAG_ANSWER){ state = FLAG_RCV; break;}
+        case START:
+            printf("start\n");
+            if (byte == FLAG_ANSWER)
+                state = FLAG_RCV;
+            else
                 state = START;
-		    break;
+            break;
 
-            case A_RCV:
-		        printf("a_rcv\n");
-                if(byte == C_ANSWER){ state = C_RCV; break;}
-		        if(byte == FLAG_ANSWER){ state = FLAG_RCV; break;}
+        case FLAG_RCV:
+            printf("flag_rcv\n");
+            if (byte == A_ANSWER)
+            {
+                state = A_RCV;
+                break;
+            }
+            if (byte == FLAG_ANSWER)
+            {
+                state = FLAG_RCV;
+                break;
+            }
+            state = START;
+            break;
+
+        case A_RCV:
+            printf("a_rcv\n");
+            if (byte == C_ANSWER)
+            {
+                state = C_RCV;
+                break;
+            }
+            if (byte == FLAG_ANSWER)
+            {
+                state = FLAG_RCV;
+                break;
+            }
+            state = START;
+            break;
+
+        case C_RCV:
+            printf("c_rcv\n");
+            if (byte == BCC_ANSWER)
+            {
+                state = BCC_OK;
+                break;
+            }
+            if (byte == FLAG_ANSWER)
+            {
+                state = FLAG_RCV;
+                break;
+            }
+            state = START;
+            break;
+
+        case BCC_OK:
+            printf("bcc_ok\n");
+            if (byte == FLAG_ANSWER)
+                state = STOP;
+            else
                 state = START;
-		    break;
-
-            case C_RCV:
-		        printf("c_rcv\n");
-                if(byte == BCC_ANSWER){ state = BCC_OK; break;}
-		        if(byte == FLAG_ANSWER){ state = FLAG_RCV; break;}
-                state = START;
-		    break;
-
-            case BCC_OK:
-		        printf("bcc_ok\n");
-                if(byte == FLAG_ANSWER) state = STOP;
-                else state = START;
-		    break;
-		   
-
+            break;
         }
-        if (state==STOP) return 1;
+        if (state == STOP)
+            return 1;
         i++;
     }
 
     return -1;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     int c;
-    struct termios oldtio,newtio;
+    struct termios oldtio, newtio;
     int i, sum = 0, speed = 0;
 
-    if ( (argc < 2) || ((strcmp("/dev/ttyS10", argv[1])!=0) &&  (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+    if ((argc < 2) || ((strcmp("/dev/ttyS10", argv[1]) != 0) && (strcmp("/dev/ttyS11", argv[1]) != 0)))
+    {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
-
 
     /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
     */
 
+    fd = open(argv[1], O_RDWR | O_NOCTTY); // abre o ficheiro e retorna o file descriptor
+    if (fd < 0)
+    {
+        perror(argv[1]);
+        exit(-1);
+    }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );     //abre o ficheiro e retorna o file descriptor
-    if (fd < 0) { perror(argv[1]); exit(-1); }
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if (tcgetattr(fd, &oldtio) == -1)
+    { /* save current port settings */
         perror("tcgetattr");
         exit(-1);
     }
@@ -166,42 +249,55 @@ int main(int argc, char** argv){
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+    newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+    newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
 
     /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) próximo(s) caracter(es)
+    leitura do(s) prÃ³ximo(s) caracter(es)
     */
 
+    tcflush(fd, TCIOFLUSH); // TCIOFLUSH - Flushes both input and output data on the terminal
 
-    tcflush(fd, TCIOFLUSH);     //TCIOFLUSH - Flushes both input and output data on the terminal
-
-    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
         perror("tcsetattr");
         exit(-1);
     }
 
     printf("New termios structure set\n");
 
-    (void) signal(SIGALRM, flag_time_out);
+    (void)signal(SIGALRM, flag_time_out);
+    /*
+    // SEND SET COMMAND
+    if (send_command(fd, set) != 1)
+        printf("ERROR SENDING SET COMMAND\n");
+    else
+        printf("SET COMMAND SENT\n");
 
-    //SEND SET COMMAND
-    if (send_command(fd,set) != 1) printf("ERROR SENDING SET COMMAND\n");
-    else printf("SET COMMAND SENT\n");
-
-    //RECEIVE UA ANSWER
-    if (read_answer(fd,ua) == 1) {
+    // RECEIVE UA ANSWER
+    if (read_answer(fd, ua) == 1)
+    {
         printf("UA answer received\n");
         alarm(0);
     }
-    else printf("ERRO ua\n");
+    else
+        printf("ERRO ua\n");
+*/
+    printf("\nSending frame\n");
 
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {     //set attributes again
+    char set_header[] = {FLAG, A, C_SET, BCC_SET};
+    if (!write_frame(fd,set_header))
+        printf("Error writing Frame\n");
+    
+    printf("\nFrame Sent :))\n");
+    
+
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    { // set attributes again
         perror("tcsetattr");
         exit(-1);
     }
-
 
     close(fd);
     return 0;
--- receiver.c
+++ receiver.c
@@ -16,149 +16,307 @@
 #define FALSE 0
 #define TRUE 1
 
-
-#define FLAG 0x5c   // header delimitation
-#define A    0x01   //Answer sent by receiver
-#define C_UA    0x07   //Control field - UA
-#define C_SET   0x03   //Control field - SET
-#define BCC_UA  (A^C_UA)
-#define BCC_SET (A^C_SET)
+#define FLAG 0x5c  // header delimitation
+#define A 0x01     // Answer sent by receiver
+#define C_UA 0x07  // Control field - UA
+#define C_SET 0x03 // Control field - SET
+#define BCC_UA (A ^ C_UA)
+#define BCC_SET (A ^ C_SET)
 
 #define FRAME_SIZE 5
 
-volatile int STOP=FALSE;
 int fd;
-
-int read_command(int fd, char* recv){
-
-    char FLAG_COMMAND = recv[0];
-    char A_COMMAND = recv[1];
-    char C_COMMAND = recv[2];
-    char BCC_COMMAND = recv[3];
-
-    //Estados
-    typedef enum {
-        START,
-        FLAG_RCV,
-        A_RCV,
-        C_RCV,
-        BCC_OK,
-        STOP
-    } statesSET;
-
-    statesSET state = START;
-    int i = 0;
-
-    char byte;
-
-    while(1){
-
-        read(fd,&byte,1); //reads chars one by one
-        printf("\nVOLTA : %d ---- Byte lido: %x\n", i,byte);
-
-        switch(state){
-
-		    case START:
-		        printf("start\n");
-		        if(byte==FLAG_COMMAND) state=FLAG_RCV;
-		        else state=START; 
-		    break;
-		   
-            case FLAG_RCV:
-		        printf("flag_rcv\n");
-		        if(byte==A_COMMAND){ state = A_RCV; break;}
-		        if (byte == FLAG_COMMAND){ state = FLAG_RCV; break;}
-                state = START;
-		    break;
-
-            case A_RCV:
-		        printf("a_rcv\n");
-                if(byte == C_COMMAND){ state = C_RCV; break;}
-		        if(byte == FLAG_COMMAND){ state = FLAG_RCV; break;}
-                state = START;
-		    break;
-
-            case C_RCV:
-		        printf("c_rcv\n");
-                if(byte == BCC_COMMAND){ state = BCC_OK; break;}
-		        if(byte == FLAG_COMMAND){ state = FLAG_RCV; break;}
-                state = START;
-		    break;
-
-            case BCC_OK:
-		        printf("bcc_ok\n");
-                if(byte == FLAG_COMMAND) state = STOP;
-                else state = START;
-		    break;
-		   
-
-        }
-        if (state==STOP) return 1;
-        i++;
-    }
-
-    return -1;
-}
-
-int send_answer(int fd, char* command){
-
-    int res;
-
-    res = write(fd,command,FRAME_SIZE);            //returns number of written bytes in the driver file and saves in res
-    printf("%d BYTES SENT\n", res);
-
-    if (res == 5) return 1;
-
-}
-int main(int argc, char** argv)
+// Estados
+typedef enum
 {
-    int c, res, res_read;
-    struct termios oldtio,newtio;
-    char buff;
+  START,
+  FLAG_RCV,
+  A_RCV,
+  C_RCV,
+  BCC_OK,
+  STOP
+} statesSET;
 
-    if ( (argc < 2) || ((strcmp("/dev/ttyS10", argv[1])!=0) &&  (strcmp("/dev/ttyS11", argv[1])!=0) )) {
-        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-        exit(1);
+statesSET state = START;
+int read_command(int fd, char *recv)
+{
+
+  char FLAG_COMMAND = recv[0];
+  char A_COMMAND = recv[1];
+  char C_COMMAND = recv[2];
+  char BCC_COMMAND = recv[3];
+
+  int i = 0;
+
+  char byte;
+
+  while (1)
+  {
+
+    read(fd, &byte, 1); // reads chars one by one
+    printf("\nVOLTA : %d ---- Byte lido: %x\n", i, byte);
+
+    switch (state)
+    {
+
+    case START:
+      printf("start\n");
+      if (byte == FLAG_COMMAND)
+        state = FLAG_RCV;
+      else
+        state = START;
+      break;
+
+    case FLAG_RCV:
+      printf("flag_rcv\n");
+      if (byte == A_COMMAND)
+      {
+        state = A_RCV;
+        break;
+      }
+      if (byte == FLAG_COMMAND)
+      {
+        state = FLAG_RCV;
+        break;
+      }
+      state = START;
+      break;
+
+    case A_RCV:
+      printf("a_rcv\n");
+      if (byte == C_COMMAND)
+      {
+        state = C_RCV;
+        break;
+      }
+      if (byte == FLAG_COMMAND)
+      {
+        state = FLAG_RCV;
+        break;
+      }
+      state = START;
+      break;
+
+    case C_RCV:
+      printf("c_rcv\n");
+      if (byte == BCC_COMMAND)
+      {
+        state = BCC_OK;
+        break;
+      }
+      if (byte == FLAG_COMMAND)
+      {
+        state = FLAG_RCV;
+        break;
+      }
+      state = START;
+      break;
+
+    case BCC_OK:
+      printf("bcc_ok\n");
+      if (byte == FLAG_COMMAND)
+        state = STOP;
+      else
+        state = START;
+      break;
+    }
+    if (state == STOP)
+      return 1;
+    i++;
+  }
+
+  return -1;
+}
+
+int read_frame(int fd, char *buf)
+{
+  int i = 0, bcc2 = 0;
+
+  char byte, dados[2000];
+
+  while (TRUE)
+  {
+    read(fd, &byte, 1);
+    printf("%x ",byte);
+    // MAQUINA DE ESTADOS DO BYTE DESTUFFING
+    /*
+    F-> A
+    A->C
+    C->BCC
+    BCC -> LER DADOS
+    LER DADOS -> FLAG
+
+    TERMINA
+    */
+
+    char FLAG_BUF = buf[0];
+    char A_BUF = buf[1];
+    char C_BUF = buf[2];
+    char BCC_BUF = buf[3];
+
+    switch (state){
+
+      case START:
+        printf("start\n");
+        if (byte == FLAG_BUF)
+          state = FLAG_RCV;
+        else
+          state = START;
+        break;
+
+      case FLAG_RCV:
+        printf("flag_rcv\n");
+        if (byte == A_BUF)
+        {
+          state = A_RCV;
+          break;
+        }
+        if (byte == FLAG_BUF)
+        {
+          state = FLAG_RCV;
+          break;
+        }
+        state = START;
+        break;
+
+      case A_RCV:
+        printf("a_rcv\n");
+        if (byte == C_BUF)
+        {
+          state = C_RCV;
+          break;
+        }
+        if (byte == FLAG_BUF)
+        {
+          state = FLAG_RCV;
+          break;
+        }
+        state = START;
+        break;
+
+      case C_RCV:
+        printf("c_rcv\n");
+        if (byte == BCC_BUF)
+        {
+          state = BCC_OK;
+          break;
+        }
+        if (byte == FLAG_BUF)
+        {
+          state = FLAG_RCV;
+          break;
+        }
+        state = START;
+        break;
+
+      case BCC_OK:
+        
+        if (byte == FLAG_BUF){ 
+          state = STOP;
+          break;
+        } 
+        dados[i] = byte;
+        if ((dados[i] == 0x7D && dados[i - 1] == 0x5D)){
+          continue;
+        }
+        
+        if ((dados[i] == 0x7C && dados[i - 1] == 0X5D)){
+          dados[i - 1] = 0x5C;
+          continue;
+        }
+        i++;
+
+        break;  
+
     }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd < 0) { perror(argv[1]); exit(-1); }
+      if (state == STOP) break;
+  }
+  
+  printf("\n\nDADOS: ");
+  bcc2 = dados[0];
+  for (int k = 1; k < i-1; k++){    //calcula o bcc2
+    printf("%x ",dados[k]);
+    bcc2 = dados[k] ^ bcc2;
+  }
+  printf("\nBCC2 calculado: %x  BCC2 lido:%x\n",bcc2,dados[i-1]);
+  if (dados[i - 1] == bcc2){
+    printf("BCC2 as expected\n");
+    return 1;
+  }
+  else{
+    printf("BCC2 is wrong\n");
+    return -1;
+  }
 
-    if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-        perror("tcgetattr");
-        exit(-1);
-    }
+  printf("\n\n --- DESTUFFED DATA ---\n\n");
+}
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+int send_answer(int fd, char *command)
+{
 
-    newtio.c_lflag = 0;
+  int res;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+  res = write(fd, command, FRAME_SIZE); // returns number of written bytes in the driver file and saves in res
+  printf("%d BYTES SENT\n", res);
 
-    tcflush(fd, TCIOFLUSH);
+  if (res == 5)
+    return 1;
+}
+int main(int argc, char **argv)
+{
+  int c, res, res_read;
+  struct termios oldtio, newtio;
+  char buff;
 
-    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
-        perror("tcsetattr");
-        exit(-1);
-    }
+  if ((argc < 2) || ((strcmp("/dev/ttyS10", argv[1]) != 0) && (strcmp("/dev/ttyS11", argv[1]) != 0)))
+  {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
 
-    printf("New termios structure set\n");
+  fd = open(argv[1], O_RDWR | O_NOCTTY);
+  if (fd < 0)
+  {
+    perror(argv[1]);
+    exit(-1);
+  }
 
-    //RECEIVES SET COMMAND
-    char set[FRAME_SIZE] =  {FLAG, A, C_SET, BCC_SET, FLAG};
-    if (read_command(fd,set) != 1) printf ("Error reading command\n"); 
-    else printf("SET state achieved\n");
+  if (tcgetattr(fd, &oldtio) == -1)
+  { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    //SENDS UA ANSWER
-    char ua[FRAME_SIZE] = {FLAG, A, C_UA, BCC_UA, FLAG};
-    if (send_answer(fd,ua) != 1) printf("ERROR SENDING UA ANSWER\n");
-    else printf("UA ANSWER SENT\n");
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
-    return 0;
+  newtio.c_lflag = 0;
+
+  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+  newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
+
+  tcflush(fd, TCIOFLUSH);
+
+  if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+  {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  printf("New termios structure set\n");
+
+  char set_buf[4] = {FLAG, A, C_SET, BCC_SET};
+
+  if (read_frame(fd,set_buf) != 1){ 
+    printf("Error reading Frame\n");
+    exit(-1);
+  }
+  printf("Frame read correctly :))\n");
+
+  tcsetattr(fd, TCSANOW, &oldtio);
+  close(fd);
+  return 0;
 }
